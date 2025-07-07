d6b7b0e8fcb5b14a040577500a3b5ea5a733ecab
Jess <jessica.sileite@gmail.com>
2025-07-07 12:13:14 -0300

    Implementadas regras classe Reserva

--- Program.cs
+++ Program.cs
@@ -6,17 +6,17 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p1 = new Pessoa(nome: "Teteu");
+Pessoa p2 = new Pessoa(nome: "Jess");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Master Blaster", capacidade: 2, valorDiaria: 300);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 20);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
